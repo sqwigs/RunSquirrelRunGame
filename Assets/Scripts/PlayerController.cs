959ba1e1ee73d7593d1396bdbc6cdf2a41fee11d
sqwigs <nate.workplace@gmail.com>
2016-09-25 19:07:56 -0600

    Worked on Player Movement Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+//[System.Serializable]
+//public class Boundary
+//{
+//    public float xMin, xMax, zMin, zMax;
+//}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class PlayerController : MonoBehaviour
+{
+    // Player Movement Control Variables
+    public float speed = 10;
+    // public Boundary bound;
+
+    // Used for movement control
+    private CharacterController charControl;
+
+    void Start ()
+    {
+        charControl = this.GetComponent<CharacterController>();
+    }
+    
+	void Update ()
+    {
+        float moveHorizontal, moveVertical;
+        
+        // get movementinput from user
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        // Restricts movement to Horizontal or Vertical, Disabling Diagonal.
+        // TODO: Fix bug were you hold both up and side causing weird direction turn. 
+        if (Mathf.Abs(moveHorizontal) > Mathf.Abs(moveVertical))
+        {
+            moveVertical = 0.0f;
+        }
+        else
+        {
+            moveHorizontal = 0.0f;
+        }
+       
+
+        // Determine vector to move character
+        Vector3 movementVector = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Move character along movementVector direction at the speed given
+        charControl.SimpleMove(movementVector * -speed);
+
+        this.transform.LookAt(this.transform.position + movementVector);
+     }
 }
