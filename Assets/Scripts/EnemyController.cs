3ad8559b19c8a9d4fb006488091cb37cf52fbeff
sqwigs <nate.workplace@gmail.com>
2016-09-25 19:22:56 -0600

    Started Enemy Movement Implementation
    Simple random movement. Need to work on correct wait time and boundaries
    of movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
+    // Speed of Enemy
+    public float speed;
+
+    // Control for the Enemy
+    private Rigidbody rigidBod;
+    public float moveWait;
+    private float waitTime;
+
 	// Use this for initialization
 	void Start () {
-	
+        rigidBod = this.GetComponent<Rigidbody>();
+        waitTime = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Time.time > moveWait)
+        {
+            waitTime = Time.time + moveWait;
+            Vector3 randVect = Random.insideUnitSphere * speed;
+
+            randVect.y = 0.0f;
+
+            // rigidBod.velocity = randVect * speed;
+
+            this.transform.Translate(randVect);
+        }
+        
+    }
 }
