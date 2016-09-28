73e0a7c9f33f2a782bec580b4cc5f772dac563c2
sqwigs <nate.workplace@gmail.com>
2016-09-27 22:16:45 -0600

    Enemy and player scripts changed
    Player moves more smoothly and I like the code a little bit better.
    Enemy movement is more fluid, but I need to figure out how to make it
    bounce correctly.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,31 +5,111 @@ public class EnemyController : MonoBehaviour {
 
     // Speed of Enemy
     public float speed;
+    public float moveWait;
+    public float bounceWait;
+    public Boundary boundary;
 
     // Control for the Enemy
     private Rigidbody rigidBod;
-    public float moveWait;
-    private float waitTime;
-
+    private double counter;
+    
 	// Use this for initialization
 	void Start () {
         rigidBod = this.GetComponent<Rigidbody>();
-        waitTime = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Time.time > moveWait)
-        {
-            waitTime = Time.time + moveWait;
-            Vector3 randVect = Random.insideUnitSphere * speed;
+        StartCoroutine(moveEnemy());
+        counter = 0.0;
+    }
 
+    //void Update ()
+    //{
+    //    float x, z;
+    //    x = findNextLocation(transform.localPosition.x, boundary.xMin, boundary.xMax);
+    //    z = findNextLocation(transform.localPosition.z, boundary.zMin, boundary.zMax);
+
+    //    transform.localPosition = new Vector3(transform.localPosition.x + x, 0.0f, transform.localPosition.z + z );
+
+    //}
+
+    //float findNextLocation (float currentPosition, float min, float max)
+    //{
+    //    if (currentPosition > max)
+    //    {
+    //        return Random.Range(-speed, 0);
+    //    }
+    //    else if (currentPosition < min)
+    //    {
+    //        return Random.Range(0, speed);
+    //    }
+
+    //    return currentPosition;
+    //}
+
+    //void Update ()
+    //{
+    //    if (counter > moveWait)
+    //    {
+
+    //        // Deterimine random direction for enemy.
+    //        Vector3 randVect = Random.insideUnitSphere * speed;
+
+    //        // make sure it does not move outside of player's plane.
+    //        randVect.y = 0.0f;
+
+    //        // set velocity trajectory for enemy.
+    //        rigidBod.velocity = randVect;
+
+    //        this.rigidBod.position = new Vector3(Mathf.Clamp(rigidBod.position.x, boundary.xMin, boundary.xMax),
+    //                                                 0.0f,
+    //                                                 Mathf.Clamp(rigidBod.position.z, boundary.zMin, boundary.zMax));
+    //        counter = 0.0;
+    //    }
+
+    //    counter += 0.1;
+
+    //}
+
+    /*
+    *   Will enumerate through the enemies movement over time.
+    */
+    IEnumerator moveEnemy()
+    {  
+        // Deterimine random direction for enemy.
+        Vector3 randVect = Random.insideUnitSphere * speed;
+        // make sure it does not move outside of player's plane.
+        randVect.y = 0.0f;
+
+        Debug.Log(randVect);
+
+        while (true)
+        {
+          
+
+            while (rigidBod.position.x > boundary.xMin && rigidBod.position.x < boundary.xMax &&
+                    rigidBod.position.z > boundary.zMin && rigidBod.position.z < boundary.zMax)
+            {
+                // set velocity trajectory for enemy.
+                rigidBod.velocity = randVect;
+               
+
+                yield return new WaitForSeconds(moveWait);
+            }
+
+            // stop rigid body and wait to bounce
+            rigidBod.velocity = Vector3.zero;
+            yield return new WaitForSeconds(bounceWait);
+
+            
+
+            // Deterimine random direction for enemy.
+            randVect = Random.insideUnitSphere * speed;
+            // make sure it does not move outside of player's plane.
             randVect.y = 0.0f;
 
-            // rigidBod.velocity = randVect * speed;
+            rigidBod.velocity = randVect;
 
-            this.transform.Translate(randVect);
+            yield return new WaitForSeconds(moveWait);
+
         }
-        
+
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,34 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//[System.Serializable]
-//public class Boundary
-//{
-//    public float xMin, xMax, zMin, zMax;
-//}
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
 
 public class PlayerController : MonoBehaviour
 {
     // Player Movement Control Variables
     public float speed = 10;
-    // public Boundary bound;
+    public Boundary boundary;
 
     // Used for movement control
     private CharacterController charControl;
+    private Rigidbody rigidBod;
+    private float moveHorizontal, moveVertical;
 
     void Start ()
     {
         charControl = this.GetComponent<CharacterController>();
+        rigidBod = this.GetComponent<Rigidbody>();
     }
     
-	void Update ()
+    void Update ()
     {
-        float moveHorizontal, moveVertical;
         
+        // Determine vector to move character
+        Vector3 movementVector = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidBod.velocity = movementVector * speed;
+
+        this.rigidBod.position = new Vector3
+        (
+            Mathf.Clamp(rigidBod.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rigidBod.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rigidBod.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+    }
+
+    // Ran only during frames
+    void FixedUpdate ()
+    {
         // get movementinput from user
         moveHorizontal = Input.GetAxis("Horizontal");
         moveVertical = Input.GetAxis("Vertical");
-
         // Restricts movement to Horizontal or Vertical, Disabling Diagonal.
         // TODO: Fix bug were you hold both up and side causing weird direction turn. 
         if (Mathf.Abs(moveHorizontal) > Mathf.Abs(moveVertical))
@@ -39,14 +57,5 @@ public class PlayerController : MonoBehaviour
         {
             moveHorizontal = 0.0f;
         }
-       
-
-        // Determine vector to move character
-        Vector3 movementVector = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        // Move character along movementVector direction at the speed given
-        charControl.SimpleMove(movementVector * -speed);
-
-        this.transform.LookAt(this.transform.position + movementVector);
-     }
+    }
 }
