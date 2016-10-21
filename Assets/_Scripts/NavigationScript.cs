ad2653ad89a757493ab6c288044a9d934f2bf30e
USERS\williamm <nate.workplace>
2016-10-21 16:55:26 -0600

    Trying to add NavAgent scripts

--- Assets/_Scripts/NavigationScript.cs
+++ Assets/_Scripts/NavigationScript.cs
@@ -1,17 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class NavigationScript : MonoBehaviour {
+public class NavigationScript : EnemyInterface {
 
-	
+    private Transform agent;
 
 	// Use this for initialization
 	void Start () {
-	
+	 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        this.GetComponent<NavMeshAgent>().setDestination(agent);
 	}
 }
