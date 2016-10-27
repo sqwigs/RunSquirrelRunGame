477b4c95a5fcbdc96409b2fd5572ad3cd5b04533
sqwigs <nate.workplace@gmail.com>
2016-10-26 23:36:20 -0600

    Created Hazard interface and started movement for spike hazard, still need to get the deactivation animation finished. Also need to finish enemy movement for coyote, wolf and power-ups.

---
+++ Assets/_Scripts/SpikeHazard.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using DG.Tweening;
+
+public class SpikeHazard : HazardInterface {
+
+    public float moveTime;
+    private Transform transform;
+
+    // Use this for initialization
+    void Start () {
+        DOTween.Init(true, false, LogBehaviour.ErrorsOnly);
+        transform = GetComponent<Transform>();
+        currTime = Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (!trapActivated)
+        {
+            trapTimer();
+        }
+    }
+
+    protected override void activateTrap()
+    {
+        transform.DOMoveY(transform.position.y + transform.lossyScale.y, moveTime);
+        Debug.Log("In activateTrap()");
+    }
+
+    protected override void deactivateTrap()
+    {
+        transform.DOMoveY(transform.position.y - transform.lossyScale.y, moveTime);
+        Debug.Log("In deactivateTrap()");
+
+    }
+}
