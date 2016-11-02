5a0d5fecb54da53253017af3c8fb97a4bfed80ab
sqwigs <nate.workplace@gmail.com>
2016-11-01 21:27:16 -0600

    Simple placeholder start screen gui

---
+++ Assets/_Scripts/UI Controls/QuitOnClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitOnClick : MonoBehaviour {
+
+    /// <summary>
+    /// Quits the current Game Application
+    /// </summary>
+	public void QuitGame ()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit ();
+        #endif
+    }
+}
