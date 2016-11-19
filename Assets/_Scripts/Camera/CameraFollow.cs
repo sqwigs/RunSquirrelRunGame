dd4e5c42e905b84c1c25afadb3edd0c1ba6b3d46
USERS\williamm <nate.workplace>
2016-11-18 17:53:25 -0700

    changed the camera following code.

--- Assets/_Scripts/Camera/CameraFollow.cs
+++ Assets/_Scripts/Camera/CameraFollow.cs
@@ -4,22 +4,31 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour
 {
 
-    private GameObject player;
+    private Transform player;
+	private float zOffset;
 
 	// Use this for initialization
 	void Start ()
     {
-        player = GameObject.Find("Player");
+        GameObject playerGameObject = GameObject.Find("Player");
+		if (playerGameObject != null) {
+			player = playerGameObject.transform;
+			zOffset = transform.position.z - player.position.z;
+		} else {
+			Debug.LogError ("Could not find \"Player\" game object");
+		}
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         if (player != null)
-        {
-            Vector3 playerpos = player.transform.position;
+        {	
+            Vector3 playerpos = player.position;
+
             playerpos.y = transform.position.y;
-            playerpos.z += 7;
+            playerpos.z += zOffset;
             transform.position = playerpos;
         }
        
